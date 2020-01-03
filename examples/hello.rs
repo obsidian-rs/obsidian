e2a300463f1158478f2f1589660b781196f398da
Wai Pai Lee <pailee.wai@gmail.com>
2020-01-03 10:30:08 +0800

    fix: add missing examples

---
+++ examples/hello.rs
@@ -0,0 +1,15 @@
+use obsidian::App;
+
+#[tokio::main]
+async fn main() {
+    let mut app = App::new();
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    app.get("/", |_ctx| async { "Hello World" });
+
+    app.listen(&addr, || {
+        {
+            println!("server is listening to {}", &addr);
+        }
+    }).await;
+}
