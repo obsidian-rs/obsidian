c0305f25c5e2b310fb9e5e7b802370d1efe0b602
Gan Jun Kai <kuhn96@gmail.com>
2020-05-07 13:22:53 +0800

    docs: add docs for Middleware trait

--- src/middleware.rs
+++ src/middleware.rs
@@ -6,6 +6,29 @@ use crate::app::EndpointExecutor;
 use crate::context::Context;
 use crate::handler::ContextResult;
 
+/// Middleware trait provides a way to allow user to:
+/// 1. alter the request data before proceeding to the next middleware or the handler
+/// 2. transform the response on the way out
+/// 3. perform side-effects before or after handler processing
+///
+/// ```
+/// use async_trait::async_trait;
+///
+/// #[async_trait]
+/// impl Middleware for ExampleMiddleware {
+///     async fn handle<'a>(
+///         &'a self,
+///         context: Context,
+///         ep_executor: EndpointExecutor<'a>,
+///     ) -> ContextResult {
+///
+///     // actions BEFORE handler processing
+///     let result = ep_executor.next(context).await?;
+///     // actions AFTER handler processing
+///
+///     Ok(result)
+/// }
+/// ```
 #[async_trait]
 pub trait Middleware: Send + Sync + 'static {
     async fn handle<'a>(
